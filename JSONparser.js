bc2d72e2b46811620eedfdfd4e34878bdd110c61
Rishabh Mishra <mishrarishabh865@gmail.com>
2016-10-17 15:16:35 +0530

    Changed functions into arrow functions

--- JSONparser.js
+++ JSONparser.js
@@ -1,32 +1,30 @@
-var inpStr = require('fs').readFileSync('example.txt').toString()
-let output = jsonParser(inpStr)
-output ? console.log(JSON.stringify(output[0], null, 2)) : console.log("Invalid JSON")
-function jsonParser (input) {
-  let parsedString
-  input = spaceParser(input)[1]
-  parsedString = nullParser(input)
-  if (parsedString) return parsedString
-  parsedString = boolParser(input)
-  if (parsedString) return parsedString
-  parsedString = numberParser(input)
-  if (parsedString) return parsedString
-  parsedString = stringParser(input)
-  if (parsedString) return parsedString
-  parsedString = objectParser(input)
-  if (parsedString) return parsedString
-  parsedString = arrayParser(input)
-  if (parsedString) return parsedString
-  return null
+let commaParser = (input) => (input[0] === ',') ? [null, input.slice(1)] : null
+let spaceParser = (input) => (input.match(/\S/)) ? [null,input.slice(input.indexOf(input.match(/\S/)))] : null
+let nullParser = (input) => (input.startsWith(null) && (input[4] === undefined || !input[4].match(/[a-zA-Z0-9]+/gi))) ? [null, input.slice(4)] : null
+
+let boolParser = (input) => {
+  if (input.startsWith(true) && (input[4] === undefined || !input[4].match(/[a-zA-Z0-9]+/gi))) return [true, input.slice(4)]
+  return (input.startsWith(false) && (input[5] === undefined || !input[5].match(/[a-zA-Z0-9]+/gi))) ? [false, input.slice(5)] : null
 }
-function commaParser (input) {
-  return (input[0] === ',') ? [null, input.slice(1)] : null
+
+let numberParser = (input) => {
+  var num = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/i
+  return (input.match(num)) ? [parseFloat(input.match(num)[0]), input.slice(input.match(num)[0].length)] : null
 }
-function arrayParser (input) {
+
+let stringParser = (input) => {
+  if (!input.startsWith('"')) return null
+  let i = 1
+  while (input[i] !== '"') (input[i] === '\\') ? i = i + 2 : i++
+  return [input.substring(1, i), input.slice(i + 1)]
+}
+
+let arrayParser = (input) => {
   if (!input.startsWith('[')) return null
   input = spaceParser(input.slice(1))[1]
   var arr = []
   while (true) {
-    var temp = jsonParser(input)
+    var temp = anyOneParser(input)
     if (!temp) return null
     arr.push(temp[0])
     input = spaceParser(temp[1])[1]
@@ -37,7 +35,8 @@ function arrayParser (input) {
   }
   return (input) ? [arr, input.slice(1)] : [arr,input]
 }
-function objectParser (input) {
+
+let objectParser = (input) => {
   if (!input.startsWith('{')) return null
   let obj = {}
   input = spaceParser(input.slice(1))[1]
@@ -47,7 +46,7 @@ function objectParser (input) {
     var [key, value] = temp
     value = spaceParser(value)[1]
     if (value.startsWith(':')) value = value.slice(1)
-    value = jsonParser(value)
+    value = anyOneParser(value)
     if (!value) return null
     obj[key] = value[0]
     input = spaceParser(value[1])[1]
@@ -58,23 +57,19 @@ function objectParser (input) {
   }
   return (input) ? [obj, input.slice(1)] : [obj, input]
 }
-function spaceParser (input) {
-  return (input.match(/\S/)) ? [null,input.slice(input.indexOf(input.match(/\S/)))] : null
+
+let jsonParser = (parsers) => {
+  return function(input) {
+      for (let i = 0; i < parsers.length; i++){
+      input = spaceParser(input)[1]
+      let res = parsers[i](input)
+      if (res) return res
+    }
+    return null
+  }
 }
-function nullParser (input) {
-  return (input.startsWith(null) && !input[4].match(/[a-zA-Z0-9]+/gi)) ? [null, input.slice(4)] : null
-}
-function boolParser (input) {
-  if (input.startsWith(true) && !input[4].match(/[a-zA-Z0-9]+/gi)) return [true, input.slice(4)]
-  return (input.startsWith(false) && !input[5].match(/[a-zA-Z0-9]+/gi)) ? [false, input.slice(5)] : null
-}
-function numberParser (input) {
-  var num = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/i
-  return (input.match(num)) ? [parseFloat(input.match(num)[0]), input.slice(input.match(num)[0].length)] : null
-}
-function stringParser (input) {
-  if (!input.startsWith('"')) return null
-  let i = 1
-  while (input[i] !== '"') (input[i] === '\\') ? i = i + 2 : i++
-  return [input.substring(1, i), input.slice(i + 1)]
-}
+
+var inpStr = require('fs').readFileSync('example.txt').toString()
+var anyOneParser = jsonParser([nullParser, boolParser, numberParser, stringParser, objectParser, arrayParser])
+let output = anyOneParser(inpStr)
+output ? console.log(JSON.stringify(output[0], null, 2)) : console.log("Invalid JSON")
