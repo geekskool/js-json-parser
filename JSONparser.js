07ccb64450783097413c6d6294e722152cdb7957
Rishabh Mishra <mishrarishabh865@gmail.com>
2016-10-24 10:34:46 +0530

    Assigned Values inside ternary operators

--- JSONparser.js
+++ JSONparser.js
@@ -1,9 +1,9 @@
 const commaParser = input => input.startsWith(',') ? [null, input.slice(1)] : null
-const spaceParser = input => input.match(/^[\s\n\r\t]/) ? [null, input.slice(input.match(/\S/).index)] : null
+const spaceParser = input => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
 const nullParser = input => input.startsWith('null') ? [null, input.slice(4)] : null
 const colonParser = input => input.startsWith(':') ? [null, input.slice(1)] : null
 const boolParser = input => input.startsWith('true') ? [true, input.slice(4)] : (input.startsWith('false') ? [false, input.slice(5)] : null)
-const numberParser = (input, num = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/i) => (input.match(num)) ? [parseFloat(input.match(num)[0]), input.slice(input.match(num)[0].length)] : null
+const numberParser = (input, num = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/i, x) => (x = input.match(num)) ? [parseFloat(x[0]), input.slice(x[0].length)] : null
 
 const stringParser = (input, i = 1) => {
   if (!input.startsWith('"')) return null
@@ -16,12 +16,11 @@ const arrayParser = (input) => {
   input = input.slice(1)
   let arr = []
   while (true) {
-    let space = spaceParser(input)
-    input = (space) ? space[1] : input
+    let space
+    input = (space = spaceParser(input)) ? space[1] : input
     input = valueParser(input)
     if (!input) { return null } arr.push(input[0])
-    space = spaceParser(input[1])
-    input = (space) ? space[1] : input[1]
+    input = (space = spaceParser(input[1])) ? space[1] : input[1]
     let temp = commaParser(input)
     if (!temp) { break } input = temp[1]
   }
@@ -33,19 +32,17 @@ const objectParser = (input) => {
   let obj = {}
   input = input.slice(1)
   while (true) {
-    let space = spaceParser(input)
-    input = (space) ? space[1] : input
+    let space
+    input = (space = spaceParser(input)) ? space[1] : input
     input = stringParser(input)
     if (!input) { return null } let [key, value] = input
-    space = spaceParser(value)
-    value = (space) ? space[1] : value
+    value = (space = spaceParser(value)) ? space[1] : value
     value = colonParser(value)
-    if (!value) { return null } space = spaceParser(value[1])
-    value = (space) ? space[1] : value
+    if (!value) { return null }
+    value = (space = spaceParser(value[1])) ? space[1] : value
     value = valueParser(value)
     if (!value) { return null } obj[key] = value[0]
-    space = spaceParser(value[1])
-    input = (space) ? space[1] : value[1]
+    input = (space = spaceParser(value[1])) ? space[1] : value[1]
     let temp = commaParser(input)
     if (!temp) { break } input = temp[1]
   }
@@ -53,7 +50,7 @@ const objectParser = (input) => {
 }
 
 const anyOneParserFactory = (...parsers) => (input) => parsers.reduce((accum, parser) => (accum === null) ? parser(input) : accum, null)
-const inpStr = require('fs').readFileSync('example.json').toString()
 const valueParser = anyOneParserFactory(nullParser, boolParser, numberParser, stringParser, objectParser, arrayParser)
+const inpStr = require('fs').readFileSync('example.json').toString()
 const output = valueParser(inpStr)
 output ? console.log(JSON.stringify(output[0], null, 2)) : console.log('Invalid JSON')
