45b8c8cce5cfba560aebc7c9b5ff7dadec506361
Rishabh Mishra <mishrarishabh865@gmail.com>
2016-10-24 08:23:00 +0530

    Changed Space and Colon Parser

--- JSONparser.js
+++ JSONparser.js
@@ -1,49 +1,59 @@
-let commaParser = (input) => (input[0] === ',') ? [null, input.slice(1)] : null
-let spaceParser = (input) => input.match(/\S/) ? [null, input.slice(input.match(/\S/).index)] : null
-let nullParser = (input) => input.startsWith('null') ? [null, input.slice(4)] : null
-let colonParser = (input) => input.match(/:/) ? [null, input.slice(input.match(/:/).index + 1)] : null
-let boolParser = (input) => input.startsWith('true') ? [true, input.slice(4)] : (input.startsWith('false') ? [false, input.slice(5)] : null)
-let numberParser = (input, num = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/i) => (input.match(num)) ? [parseFloat(input.match(num)[0]), input.slice(input.match(num)[0].length)] : null
+const commaParser = input => input.startsWith(',') ? [null, input.slice(1)] : null
+const spaceParser = input => input.match(/^[\s\n\r\t]/) ? [null, input.slice(input.match(/\S/).index)] : null
+const nullParser = input => input.startsWith('null') ? [null, input.slice(4)] : null
+const colonParser = input => input.startsWith(':') ? [null, input.slice(1)] : null
+const boolParser = input => input.startsWith('true') ? [true, input.slice(4)] : (input.startsWith('false') ? [false, input.slice(5)] : null)
+const numberParser = (input, num = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/i) => (input.match(num)) ? [parseFloat(input.match(num)[0]), input.slice(input.match(num)[0].length)] : null
 
-let stringParser = (input, i = 1) => {
+const stringParser = (input, i = 1) => {
   if (!input.startsWith('"')) return null
   while (input[i] !== '"') (input[i] === '\\') ? i = i + 2 : i++
   return [input.substring(1, i), input.slice(i + 1)]
 }
 
-let arrayParser = (input) => {
+const arrayParser = (input) => {
   if (!input.startsWith('[')) return null
   input = input.slice(1)
   let arr = []
   while (true) {
-    input = valueParser(spaceParser(input)[1])
+    let space = spaceParser(input)
+    input = (space) ? space[1] : input
+    input = valueParser(input)
     if (!input) { return null } arr.push(input[0])
-    input = spaceParser(input[1])[1]
+    space = spaceParser(input[1])
+    input = (space) ? space[1] : input[1]
     let temp = commaParser(input)
     if (!temp) { break } input = temp[1]
   }
-  return (input) ? [arr, input.slice(1)] : [arr, input]
+  return (input.startsWith(']')) ? [arr, input.slice(1)] : null
 }
 
-let objectParser = (input) => {
+const objectParser = (input) => {
   if (!input.startsWith('{')) return null
   let obj = {}
   input = input.slice(1)
   while (true) {
-    input = stringParser(spaceParser(input)[1])
+    let space = spaceParser(input)
+    input = (space) ? space[1] : input
+    input = stringParser(input)
     if (!input) { return null } let [key, value] = input
-    value = spaceParser(colonParser(value)[1])[1]
+    space = spaceParser(value)
+    value = (space) ? space[1] : value
+    value = colonParser(value)
+    if (!value) { return null } space = spaceParser(value[1])
+    value = (space) ? space[1] : value
     value = valueParser(value)
     if (!value) { return null } obj[key] = value[0]
-    input = spaceParser(value[1])[1]
+    space = spaceParser(value[1])
+    input = (space) ? space[1] : value[1]
     let temp = commaParser(input)
     if (!temp) { break } input = temp[1]
   }
-  return (input) ? [obj, input.slice(1)] : [obj, input]
+  return (input.startsWith('}')) ? [obj, input.slice(1)] : null
 }
 
-let anyOneParserFactory = (...parsers) => (input) => parsers.reduce((accum, parser) => (accum === null) ? parser(input) : accum, null)
-let inpStr = require('fs').readFileSync('example.json').toString()
-let valueParser = anyOneParserFactory(nullParser, boolParser, numberParser, stringParser, objectParser, arrayParser)
-let output = valueParser(inpStr)
+const anyOneParserFactory = (...parsers) => (input) => parsers.reduce((accum, parser) => (accum === null) ? parser(input) : accum, null)
+const inpStr = require('fs').readFileSync('example.json').toString()
+const valueParser = anyOneParserFactory(nullParser, boolParser, numberParser, stringParser, objectParser, arrayParser)
+const output = valueParser(inpStr)
 output ? console.log(JSON.stringify(output[0], null, 2)) : console.log('Invalid JSON')
